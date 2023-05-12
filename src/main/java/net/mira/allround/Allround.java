155886da6e0cbb168fa7e9558d6625e2b2396066
MiraibiitwitchDE <maximilianbiebl2002@gmail.com>
2023-05-12 22:05:09 +0200

    Qualitiy changes

--- src/main/java/net/mira/allround/Allround.java
+++ src/main/java/net/mira/allround/Allround.java
@@ -1,18 +1,24 @@
 package net.mira.allround;
 
+import net.mira.allround.Utils.ConfigFile;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 public class Allround extends JavaPlugin {
     Logger log = getLogger();
+    File file = ConfigFile.getFile(this);
+    YamlConfiguration config = ConfigFile.getConfig(file);
 
     @Override
     public void onEnable() {
         log.info("Allround plugin enabled!");
-        var homeManager = new HomeManager();
+        var homeManager = new HomeManager(this);
         getCommand("sethome").setExecutor(new SetHomeCommand(homeManager));
         getCommand("home").setExecutor(new HomeCommand(homeManager));
+        getCommand("delhome").setExecutor(new DeleteHomeCommand(homeManager));
         //do something
     }
 
