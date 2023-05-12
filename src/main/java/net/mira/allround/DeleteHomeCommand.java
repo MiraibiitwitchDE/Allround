5cf58249afb26bfce8a3ca91bf8dfa6e91289a49
MiraibiitwitchDE <maximilianbiebl2002@gmail.com>
2023-05-13 01:26:00 +0200

    improved the command /delhome
    I improved the function /delhome so that no error message happens when executing the command

--- src/main/java/net/mira/allround/DeleteHomeCommand.java
+++ src/main/java/net/mira/allround/DeleteHomeCommand.java
@@ -1,6 +1,7 @@
 package net.mira.allround;
 
 import net.kyori.adventure.text.Component;
+import net.mira.allround.Utils.ConfigFile;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,24 +9,33 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import java.io.File;
+
 public class DeleteHomeCommand implements CommandExecutor {
 
     private HomeManager homeManager;
+    private Allround plugin;
 
     public DeleteHomeCommand(HomeManager homeManager) {
         this.homeManager = homeManager;
+        this.plugin = plugin;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof ConsoleCommandSender) {
-            sender.sendMessage(ChatColor.RED + "This command can only be used by one player!");
+            sender.sendMessage(ChatColor.RED + "This command can only be used by players!");
             return true;
         }
 
         if (sender instanceof Player player) {
             homeManager.delHomeLocation(player);
             player.sendMessage(Component.text(ChatColor.RED + "Your home point has been removed!"));
+
+            // Delete key from homes.yml
+            File configFile = ConfigFile.getFile(plugin);
+            ConfigFile.deleteKey(configFile, player.getUniqueId().toString());
+            homeManager.reloadHomeLocations();
             return true;
         }
 
